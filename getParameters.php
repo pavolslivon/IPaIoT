71d567572f1999dbea8d19b3dd9296b2c8827aac
pavolslivon <114649807+pavolslivon@users.noreply.github.com>
2022-11-05 12:29:17 +0100

    Update getParameters.php

--- getParameters.php
+++ getParameters.php
@@ -3,8 +3,7 @@
         
         $sn1 = $_GET["a"];
         $sn2 = $_GET["b"];
-        a = 1;
-        b = 2;
+
         $text = "a=" . $sn1 . " b=" . $sn2;
         $sum = $sn1 + $sn2;
         
