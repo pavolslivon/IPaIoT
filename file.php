6d3ab97bc29135db4f49fb0ff371653d32b8f183
Pavo <pavol.slivon@student.tuke.sk>
2022-11-11 12:46:51 +0100

    asdasd

--- file.php
+++ file.php
@@ -69,27 +69,21 @@
     }
     ?>
 
-    <h2>PHP Form Validation Example</h2>
-    <p><span class="error">* required field</span></p>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-        Name: <input type="text" name="name" value="<?php echo $name; ?>">
-        <span class="error">* <?php echo $nameErr; ?></span>
-        <br><br>
-        E-mail: <input type="text" name="email" value="<?php echo $email; ?>">
-        <span class="error">* <?php echo $emailErr; ?></span>
-        <br><br>
-        Website: <input type="text" name="website" value="<?php echo $website; ?>">
-        <span class="error"><?php echo $websiteErr; ?></span>
-        <br><br>
-        Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment; ?></textarea>
-        <br><br>
-        Gender:
-        <input type="radio" name="gender" <?php if (isset($gender) && $gender == "female") echo "checked"; ?> value="female">Female
-        <input type="radio" name="gender" <?php if (isset($gender) && $gender == "male") echo "checked"; ?> value="male">Male
-        <input type="radio" name="gender" <?php if (isset($gender) && $gender == "other") echo "checked"; ?> value="other">Other
-        <span class="error">* <?php echo $genderErr; ?></span>
-        <br><br>
-        <input type="submit" name="submit" value="Submit">
+    <div class="user-box">
+        <input type="text" name="" required="">
+        <label>Username</label>
+    </div>
+    <div class="user-box">
+        <input type="password" name="" required="">
+        <label>Password</label>
+    </div>
+    <a href="#">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        Submit
+    </a>
     </form>
 
     <?php
