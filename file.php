a7fb01c76f16a6124fbed27f052d6cb8de4f4fa1
Pavo <pavol.slivon@student.tuke.sk>
2022-11-11 13:46:36 +0100

    asdasd

--- file.php
+++ file.php
@@ -6,11 +6,15 @@
 </head>
 
 <body>
+    <?php
+    // define variables and set to empty values
+    $name = $email = $gender = $comment = $website = "";
+    ?>
     <div class="login-box">
         <h2>Login</h2>
         <form>
             <div class="user-box">
-                <input type="text" name="" required="">
+                Name: <input type="text" name="" required="" <?php echo $name; ?>">
                 <label>Username</label>
             </div>
             <div class="user-box">
@@ -26,6 +30,13 @@
             </a>
         </form>
     </div>
+
+    <?php
+    echo "<h2>Your Input:</h2>";
+    echo $name;
+    echo "<br>";
+    ?>
+
 </body>
 
 </html>
