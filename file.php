998821a7b477a1d165d09927895107622d7c9894
Pavo <pavol.slivon@student.tuke.sk>
2022-11-11 14:03:29 +0100

    asdasd

--- file.php
+++ file.php
@@ -8,7 +8,6 @@
 <body>
     <?php
     // define variables and set to empty values
-    // define variables and set to empty values
     $nameErr = $emailErr = $genderErr = $websiteErr = "";
     $name = $email = $gender = $comment = $website = "";
 
@@ -32,38 +31,22 @@
         return $data;
     }
     ?>
-    <h2>PHP Form Validation Example232332</h2>
-    <p><span class="error">* required field</span></p>
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-        Name: <input type="text" name="name" value="<?php echo $name; ?>">
-        <span class="error">*
-            <?php echo $nameErr; ?>
-        </span>
-        <br><br>
-        E-mail: <input type="text" name="email" value="<?php echo $email; ?>">
-        <span class="error">*
-            <?php echo $emailErr; ?>
-        </span>
-        <br><br>
-        Website: <input type="text" name="website" value="<?php echo $website; ?>">
-        <span class="error">
-            <?php echo $websiteErr; ?>
-        </span>
-        <br><br>
-        Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment; ?></textarea>
-        <br><br>
-        Gender:
-        <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked"; ?>
-        value="female">Female
-        <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked"; ?>
-        value="male">Male
-        <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked"; ?>
-        value="other">Other
-        <span class="error">*
-            <?php echo $genderErr; ?>
-        </span>
-        <br><br>
-        <input type="submit" name="submit" value="Submit">
+        <div class="login-box">
+            <h2>Login</h2>
+            <form>
+                <div class="user-box">
+                    <input type="text" name="" required="" <?php echo $name; ?>">
+                    <label>Username</label>
+                </div>
+                <div class="user-box">
+                    <input type="password" name="" required="">
+                    <label>Password</label>
+                </div>
+                <input type="submit" name="submit" value="Submit">
+                </a>
+            </form>
+        </div>
     </form>
 
     <?php
